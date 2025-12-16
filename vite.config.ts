0d26b29d26f5bcfba6db8d519503c8334c9e03d5
VASSA <baccabich@mail.ru>
2025-12-16 20:33:49 +0900

    deploy-fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/sonnet-react/',
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  root: '.', 
+  publicDir: 'public', 
 })
